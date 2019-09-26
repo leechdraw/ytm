af6b5008c3e4f77e3b83fdd13d8ff0ca03f81f06
vklyapov <vklyapov@oneincsystems.com>
2019-09-26 16:03:34 +0700

    Task ^YM-2 placeholder created

---
+++ scripts/instal.ps1
@@ -0,0 +1,7 @@
+$ScriptVersion = 1
+# get cur dir
+# check if newer version of this script exists
+# find if install info exists
+# if not - download last version of archive
+# if exists - check date of install version and download latest if need
+
