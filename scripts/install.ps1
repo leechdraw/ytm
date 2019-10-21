d75a5dcf2834dbd1a0cac585970aedd2073fc659
vklyapov <vklyapov@oneincsystems.com>
2019-10-21 11:29:15 +0700

    YM-1 minnor progress in install script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -10,6 +10,12 @@ $githubUri = "https://raw.githubusercontent.com/leechdraw/ytm/dev/scripts/instal
 # if exists - check date of install version and download latest if need
 
 $workDir = Resolve-Path $PSScriptRoot
+$rootLogDir = Join-Path $workDir "logs"
+$scriptLogDir = Join-Path $rootLogDir "installer"
+if(!(Test-Path $scriptLogDir))
+{
+    New-Item -Path $scriptLogDir -Force -ErrorAction Stop -ItemType Directory
+}
 
 # functions
 function ExtractVersionFromScript {
@@ -24,6 +30,29 @@ function ExtractVersionFromScript {
     return 0
 }
 
+function wLog {
+    param (
+        [string]$text,
+        [bool]$isError = $false
+    )
+    if($isError)
+    {
+        Write-Error "ERROR: $text"
+    }
+    else 
+    {
+        Write-Host $text    
+    }
+    
+    $outputFile = Join-Path $scriptLogDir "run.txt"
+    $outputD = @{
+        "Date" = (Get-Date)
+        "Text" = $text
+        "IsError" = $isError
+    } | ConvertTo-Json -Depth 32 -Compress
+    $outputD | Out-File -Append -FilePath $outputFile -ErrorAction Stop
+}
+
 function UpdateScriptVersionIfNeed {
     $repoScript = (Invoke-WebRequest -Uri $githubUri).Content
     $repoVersionOfScript = ExtractVersionFromScript -content $repoScript
@@ -36,17 +65,17 @@ function UpdateScriptVersionIfNeed {
             $tmpVersion = ExtractVersionFromScript $tmpFileContent
             if($tmpVersion -eq $repoVersionOfScript)
             {
-                Write-Host "Please, rename file [new_install.ps1] into [install.ps1]"
+                wLog "Please, rename file [new_install.ps1] into [install.ps1]"
                 exit 0
             }
             Remove-Item $newVersionFile -Force -ErrorAction Stop            
         }
         $repoScript | Set-Content -Path $newVersionFile -ErrorAction Stop -Force -Encoding utf8
-        Write-Host "Install script was updated from Version [$ScriptVersion`] to [$repoVersionOfScript]."
-        Write-Host "Please, rename file [new_install.ps1] into [install.ps1]"
+        wLog "Install script was updated from Version [$ScriptVersion`] to [$repoVersionOfScript]."
+        wLog "Please, rename file [new_install.ps1] into [install.ps1]"
         exit 0
     }
-    Write-Host "Install script has latest version!"
+    wLog "Install script has latest version!"
 }
 
 function CheckFolderStructure 
@@ -54,9 +83,14 @@ function CheckFolderStructure
     $folders = "mp3", "video", "ffmpeg"
     $folders = $folders | ForEach-Object{Join-Path $workDir $_}
     $folders | ForEach-Object {
-        if(!(Test-Path $_))
+        if(Test-Path $_)
+        {
+            wLog "$_ exists!"
+        }
+        else
         {
             New-Item -ItemType Directory -Path $_ -Force
+            wLog "$_ created!"
         }
     }
 }
